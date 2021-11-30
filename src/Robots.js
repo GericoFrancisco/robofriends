5cb7a7a185f6d2aeb6741e3cfe7364517673d6d0
gericofrancisco <gericofrancisco@gmail.com>
2021-11-30 20:27:05 +0800

    initial commit

---
+++ src/Robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id:1,
+        name: 'Leonard Hofstadter',
+        username: 'Lenny',
+        email: 'lenlen@gmail.com'
+    },
+    {
+        id:2,
+        name: 'Sheldon Cooper',
+        username: 'Sheldor',
+        email: 'sheldon@gmail.com'
+    },
+    {
+        id:3,
+        name: 'Raj Koothrapalli',
+        username: 'Rajesh',
+        email: 'raj@gmail.com'
+    },
+    {
+        id:4,
+        name: 'Howard Wolowits',
+        username: 'Wolowizard',
+        email: 'howie@gmail.com'
+    },
+    {
+        id:5,
+        name: 'Penny Something',
+        username: 'Painy',
+        email: 'penpen@gmail.com'
+    },
+    {
+        id:6,
+        name: 'Amy Fowler',
+        username: 'Amywamy',
+        email: 'fowler@gmail.com'
+    },
+    {
+        id:7,
+        name: 'Bernadeth Rostenkowski',
+        username: 'Bernie',
+        email: 'bernie@gmail.com'
+    },
+    {
+        id:8,
+        name: 'Stuart Moot',
+        username: 'Stewie',
+        email: 'stew@gmail.com'
+    },
+    {
+        id:9,
+        name: 'Bert Sanders',
+        username: 'Rocky',
+        email: 'bert@gmail.com'
+    },
+    {
+        id:10,
+        name: 'Prof Gablehowser',
+        username: 'Gabe',
+        email: 'gabe@gmail.com'
+    },
+]
